db218e754956d40a2fa282d40aeb9b0976d6a8c1
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-04-11 17:14:22 +0700

    Update register interface

---
+++ client/src/pages/register/index.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./style.css";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!username || !email || !phoneNumber || !password || !confirmPassword)
+      alert("Vui lòng điền đầy đủ thông tin.");
+    else {
+      if (password !== confirmPassword) {
+        alert("Mật khẩu xác nhận không đúng");
+        return;
+      }
+      if (password.length < 6) {
+        alert("Mật khẩu có độ dài tối thiểu là 6");
+        return;
+      }
+      const response = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          phoneNumber,
+          password,
+        }),
+      });
+      const data = await response.json();
+      alert(data.message);
+      if (response.ok) navigate("/login");
+    }
+  };
+
+  return (
+    <div className="container py-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <div className="card shadow-lg register-form">
+            <h1 className="text-center mb-4">Register</h1>
+            <div className="card-body p-4">
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <label htmlFor="username" className="form-label">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    className="form-control"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="form-control"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="phone-number" className="form-label">
+                    Phone number
+                  </label>
+                  <input
+                    type="tel"
+                    id="phone-number"
+                    className="form-control"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="confirmPassword" className="form-label">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    className="form-control"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+                <div className="text-center">
+                  <button type="submit" className="btn btn-primary mt-2">
+                    Register
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
