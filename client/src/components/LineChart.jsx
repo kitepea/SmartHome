6fc0e7c39e6781cba1f2635e5f68ad30c5377be1
nguyentrongtinbku <108884897+nguyentrongtinbku@users.noreply.github.com>
2024-04-13 13:54:34 +0700

    Room interface

---
+++ client/src/components/LineChart.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const Chart = ({ data, ynames, domains }) => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const handleDateChange = date => {
+        setSelectedDate(date);
+    };
+
+    return (
+        <div class = "container">
+            <div class = "ms-5 mb-2">
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={handleDateChange}
+                    dateFormat="dd/MM/yyyy"
+                />
+            </div>
+            <LineChart width={600} height={300} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" label={{ value: 'Hour', position: 'insideBottomRight', dy:10 }} interval={1} />
+                <YAxis domain={domains} label={{ angle: -90, position: 'insideLeft' }} />
+                <Tooltip />
+                {ynames.map((yname, index) => (
+                    <Line key={index} type="monotone" dataKey={yname.key} stroke={yname.color} name={yname.label} />
+                ))}
+                <Legend />
+            </LineChart>
+        </div>
+    );
+};
+
+export default Chart;
