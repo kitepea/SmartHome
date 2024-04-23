8fd7aab9bd8dc3c69248addcc0adc2fb67ad6e75
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-04-23 10:10:25 +0700

    Update: Register html tag

--- client/src/pages/register/index.jsx
+++ client/src/pages/register/index.jsx
@@ -43,79 +43,81 @@ const Register = () => {
   };
 
   return (
-    <div className="container py-5">
-      <div className="row justify-content-center">
-        <div className="col-lg-6">
-          <div className="card shadow-lg register-form">
-            <h1 className="text-center mb-4">Register</h1>
-            <div className="card-body p-4">
-              <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                  <label htmlFor="username" className="form-label">
-                    Username
-                  </label>
-                  <input
-                    type="text"
-                    id="username"
-                    className="form-control"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="form-control"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="phone-number" className="form-label">
-                    Phone number
-                  </label>
-                  <input
-                    type="tel"
-                    id="phone-number"
-                    className="form-control"
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="form-control"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="confirmPassword" className="form-label">
-                    Confirm Password
-                  </label>
-                  <input
-                    type="password"
-                    id="confirmPassword"
-                    className="form-control"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                  />
-                </div>
-                <div className="text-center">
-                  <button type="submit" className="btn btn-primary mt-2">
-                    Register
-                  </button>
-                </div>
-              </form>
+    <div className="register-body">
+      <div className="container py-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-6">
+            <div className="card shadow-lg register-form">
+              <h1 className="text-center mb-4">Register</h1>
+              <div className="card-body p-4">
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-3">
+                    <label htmlFor="username" className="form-label">
+                      Username
+                    </label>
+                    <input
+                      type="text"
+                      id="username"
+                      className="form-control"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      className="form-control"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="phone-number" className="form-label">
+                      Phone number
+                    </label>
+                    <input
+                      type="tel"
+                      id="phone-number"
+                      className="form-control"
+                      value={phoneNumber}
+                      onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      className="form-control"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="confirmPassword" className="form-label">
+                      Confirm Password
+                    </label>
+                    <input
+                      type="password"
+                      id="confirmPassword"
+                      className="form-control"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                  </div>
+                  <div className="text-center">
+                    <button type="submit" className="btn btn-primary mt-2">
+                      Register
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
